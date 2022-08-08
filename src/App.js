d941a1637de145e130c7f88e3ca0f0dd1c18e1cd
tisukRoman <tisukroman@gmail.com>
2022-08-08 17:25:40 +0300

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div>App</div>;
-}
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Navbar';
+
+const App = () => {
+  return (
+    <Box>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/exercise/:id' element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
+};
 
 export default App;
