44a8bac5abf070fbd7f53246c86084c3d54b7785
tisukRoman <tisukroman@gmail.com>
2022-08-12 16:35:02 +0300

    deploy to gh-pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { BodyPartsProvider } from './context/BodyParts';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <BodyPartsProvider>
       <App />
     </BodyPartsProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
